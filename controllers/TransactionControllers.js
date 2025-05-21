1522cc8b65e599b50c954f9dcc5616e8f9607ef5
Fabrian28 <fabrianrafi@outlook.com>
2025-05-21 14:37:36 +0700

    feat: added transaction api

--- controllers/TransactionControllers.js
+++ controllers/TransactionControllers.js
@@ -1,9 +1,20 @@
 const { sequelize, Time_Slot, Order, Order_Item, Menu_Item } = require("../models");
 
 class TransactionController {
-    static async getOrders(req, res, next) {
+    static async getTransactionHistory(req, res, next) {
         try {
             const orders = await Order.findAll();
+            res.status(200).json(orders);
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async getTransactionHistoryById(req, res, next) {
+        try {
+            const { id } = req.params;
+
+            const orders = await Order.findByPk(id);
             res.status(200).json(orders);
         } catch (error) {
             next(error)
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,33 @@
-const ProductControllers = require('../controllers/ProductControllers');
-const TimeSlotControllers = require('../controllers/TimeSlotControllers');
-const TransactionController = require('../controllers/TransactionControllers');
-const UserControllers = require('../controllers/UserControllers');
-const CartControllers = require('../controllers/CartControllers');
-const authentication = require('../middleware/authentication');
-const route = require('express').Router();
+    const ProductControllers = require('../controllers/ProductControllers');
+    const TimeSlotControllers = require('../controllers/TimeSlotControllers');
+    const TransactionController = require('../controllers/TransactionControllers');
+    const UserControllers = require('../controllers/UserControllers');
+    const CartControllers = require('../controllers/CartControllers');
+    const authentication = require('../middleware/authentication');
+    const route = require('express').Router();
 
-route.get('/', (req, res) => {
-    res.send('TEST ROUTING');
-})
+    route.get('/', (req, res) => {
+        res.send('TEST ROUTING');
+    })
 
-// User routes
-route.post('/api/register', UserControllers.userRegister);
-route.post('/api/login', UserControllers.userLogin);
+    // User routes
+    route.post('/api/register', UserControllers.userRegister);
+    route.post('/api/login', UserControllers.userLogin);
 
-// Product routes
-route.get('/api/menu', ProductControllers.getAllMenuItems);
-route.get('/api/slots', TimeSlotControllers.getAllSlotItems);
+    // Product routes
+    route.get('/api/menu', ProductControllers.getAllMenuItems);
+    route.get('/api/menu/:id', ProductControllers.getMenuItemById);
 
-// Cart routes
-route.get('/api/cart', authentication ,CartControllers.getCart);
-route.post('/api/cart', authentication ,CartControllers.addToCart);
-route.delete('/api/cart', authentication ,CartControllers.removeFromCart);
+    // Time Slot routes
+    route.get('/api/slots', TimeSlotControllers.getAllSlotItems);
 
+    // Cart routes
+    route.get('/api/cart', authentication ,CartControllers.getCart);
+    route.post('/api/cart', authentication ,CartControllers.addToCart);
+    route.delete('/api/cart', authentication ,CartControllers.removeFromCart);
 
-route.get('/api/order', TransactionController.getOrders);
+    // Order routes
+    route.get('/api/order/history', authentication, TransactionController.getTransactionHistory);
+    route.get('/api/order/:id', authentication, TransactionController.getTransactionHistoryById);
 
-
-module.exports = route;
+    module.exports = route;
