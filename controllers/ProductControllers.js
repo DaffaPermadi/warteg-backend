99f7805824a23c77f18a23ffaf48b4e34ad4b1a7
DaffaPermadi <daffagt123@gmail.com>
2025-05-21 15:34:18 +0700

    resolve stash

--- controllers/ProductControllers.js
+++ controllers/ProductControllers.js
@@ -1,50 +1,57 @@
-const { Menu_Item }  = require('../models');
+const { Menu_Item } = require("../models");
 
 class ProductControllers {
-    static async getAllMenuItems(req, res, next) {
-        try {
-            const menuItems = await Menu_Item.findAll();
-            res.status(200).json(menuItems);
-        } catch (error) {
-            next(error)
-        }
+  static async getAllMenuItems(req, res, next) {
+    try {
+      const menuItems = await Menu_Item.findAll();
+      res.status(200).json(menuItems);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    static async getMenuItemById(req, res, next) {
-        try {
-            const { id } = req.params;
-            const menuItem = await Menu_Item.findByPk(id);
-            if (!menuItem) {
-                return res.status(404).json({ message: 'Menu item not found' });
-            }
-            res.status(200).json(menuItem);
-        } catch (error) {
-            next(error)
-        }
+  static async getMenuItemById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const menuItem = await Menu_Item.findByPk(id);
+      if (!menuItem) {
+        return res.status(404).json({ message: "Menu item not found" });
+      }
+      res.status(200).json(menuItem);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    static async addRatingById(req, res, next) {
-        try {
-            const { id } = req.params;
-            const { rating } = req.body;
+  static async addRatingById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { rating } = req.body;
 
-            if (!rating) {
-                return res.status(400).json({ message: 'Rating is required' });
-            }
+      if (!rating || isNaN(rating) || rating < 1 || rating > 5) {
+        return res
+          .status(400)
+          .json({ message: "Rating must be between 1 and 5" });
+      }
 
-            const menuItem = await Menu_Item.findByPk(id);
-            if (!menuItem) {
-                return res.status(404).json({ message: 'Menu item not found' });
-            }
+      const menuItem = await Menu_Item.findByPk(id);
+      if (!menuItem) {
+        return res.status(404).json({ message: "Menu item not found" });
+      }
 
-            menuItem.rating = rating;
-            await menuItem.save();
+      menuItem.total_rating += rating;
+      menuItem.rating_count += 1;
+      // Tidak perlu update rating manual karena sudah ada hook di model
+      await menuItem.save();
 
-            res.status(200).json(menuItem);
-        } catch (error) {
-            next(error)
-        }
+      res.status(200).json({
+        message: "Rating updated successfully",
+        rating: menuItem.rating,
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 }
 
 module.exports = ProductControllers;
