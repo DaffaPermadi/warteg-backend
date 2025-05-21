159f1433d141d7c4f8e3cc45cee12ade91e083ec
DaffaPermadi <daffagt123@gmail.com>
2025-05-21 14:42:27 +0700

    resolve conflict

--- controllers/ProductControllers.js
+++ controllers/ProductControllers.js
@@ -22,6 +22,29 @@ class ProductControllers {
             next(error)
         }
     }
+
+    static async addRatingById(req, res, next) {
+        try {
+            const { id } = req.params;
+            const { rating } = req.body;
+
+            if (!rating) {
+                return res.status(400).json({ message: 'Rating is required' });
+            }
+
+            const menuItem = await Menu_Item.findByPk(id);
+            if (!menuItem) {
+                return res.status(404).json({ message: 'Menu item not found' });
+            }
+
+            menuItem.rating = rating;
+            await menuItem.save();
+
+            res.status(200).json(menuItem);
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 module.exports = ProductControllers;
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,43 @@
-    const ProductControllers = require('../controllers/ProductControllers');
-    const TimeSlotControllers = require('../controllers/TimeSlotControllers');
-    const TransactionController = require('../controllers/TransactionControllers');
-    const UserControllers = require('../controllers/UserControllers');
-    const CartControllers = require('../controllers/CartControllers');
-    const authentication = require('../middleware/authentication');
-    const route = require('express').Router();
+const ProductControllers = require("../controllers/ProductControllers");
+const TimeSlotControllers = require("../controllers/TimeSlotControllers");
+const TransactionController = require("../controllers/TransactionControllers");
+const UserControllers = require("../controllers/UserControllers");
+const CartControllers = require("../controllers/CartControllers");
+const authentication = require("../middleware/authentication");
+const route = require("express").Router();
 
-    route.get('/', (req, res) => {
-        res.send('TEST ROUTING');
-    })
+route.get("/", (req, res) => {
+  res.send("TEST ROUTING");
+});
 
-    // User routes
-    route.post('/api/register', UserControllers.userRegister);
-    route.post('/api/login', UserControllers.userLogin);
+// User routes
+route.post("/api/register", UserControllers.userRegister);
+route.post("/api/login", UserControllers.userLogin);
 
-    // Product routes
-    route.get('/api/menu', ProductControllers.getAllMenuItems);
-    route.get('/api/menu/:id', ProductControllers.getMenuItemById);
+// Product routes
+route.get("/api/menu", ProductControllers.getAllMenuItems);
+route.get("/api/slots", TimeSlotControllers.getAllSlotItems);
+route.get("/api/menu/:id", ProductControllers.getMenuItemById);
+route.post("/api/menu/:id/rating", ProductControllers.addRatingById);
 
-    // Time Slot routes
-    route.get('/api/slots', TimeSlotControllers.getAllSlotItems);
+// Time Slot routes
+route.get("/api/slots", TimeSlotControllers.getAllSlotItems);
 
-    // Cart routes
-    route.get('/api/cart', authentication ,CartControllers.getCart);
-    route.post('/api/cart', authentication ,CartControllers.addToCart);
-    route.delete('/api/cart', authentication ,CartControllers.removeFromCart);
+// Cart routes
+route.get("/api/cart", authentication, CartControllers.getCart);
+route.post("/api/cart", authentication, CartControllers.addToCart);
+route.delete("/api/cart", authentication, CartControllers.removeFromCart);
 
-    // Order routes
-    route.get('/api/order/history', authentication, TransactionController.getTransactionHistory);
-    route.get('/api/order/:id', authentication, TransactionController.getTransactionHistoryById);
+// Order routes
+route.get(
+  "/api/order/history",
+  authentication,
+  TransactionController.getTransactionHistory
+);
+route.get(
+  "/api/order/:id",
+  authentication,
+  TransactionController.getTransactionHistoryById
+);
 
-    module.exports = route;
+module.exports = route;
